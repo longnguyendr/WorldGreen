a7470d8fde837484060cbc3dc711024cd43424a8
longnguyendr <long.nguyendr@gmail.com>
2019-03-18 13:19:50 +0200

    Update FirebaseManager.java

--- app/src/main/java/com/example/worldgreen/FirebaseManager.java
+++ app/src/main/java/com/example/worldgreen/FirebaseManager.java
@@ -8,5 +8,23 @@ public class FirebaseManager {
         String Hey_testing;
         Hey_testing = "Just testing";
     }
+    String getEvent() {
+        return name;
+    }
+    String getName() {
+        return name;
+    }
+
+    void sayHi() {
+        System.out.println("hi");
+    }
+
+    void sayHi2() {
+        System.out.println("hi");
+    }
+    void sayHi3() {
+        System.out.println("hi");
+    }
+
 
 }
