0195c270a601cad86c6ca684a57d30ba84abc781
longnguyendr <long.nguyendr@gmail.com>
2019-03-19 13:14:11 +0200

    Update

--- app/src/main/java/com/example/worldgreen/Events/AllEventActivity.java
+++ app/src/main/java/com/example/worldgreen/Events/AllEventActivity.java
@@ -1,13 +1,15 @@
-package com.example.worldgreen;
+package com.example.worldgreen.Events;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class DetailEventActivity extends AppCompatActivity {
+import com.example.worldgreen.R;
+
+public class AllEventActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail_event);
+        setContentView(R.layout.activity_all_event);
     }
 }
--- app/src/main/java/com/example/worldgreen/Users/LoginActivity.java
+++ app/src/main/java/com/example/worldgreen/Users/LoginActivity.java
@@ -1,4 +1,4 @@
-package com.example.worldgreen;
+package com.example.worldgreen.Users;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,6 +10,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.worldgreen.MainActivity;
+import com.example.worldgreen.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
