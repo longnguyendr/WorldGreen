ebb7e91bb612ddda2a4dc3a83f5a2830895cacb5
xpolach4 <rachel.polachova@gmail.com>
2019-04-01 10:22:56 +0300

    signing test 3

---
+++ app/src/main/java/com/example/worldgreen/Events/MyEventActivity.java
@@ -0,0 +1,15 @@
+package com.example.worldgreen.Events;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.worldgreen.R;
+
+public class MyEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_event);
+    }
+}
