fb1c45af2a6d002a3f280d1e7e0758fb5c4ea61d
long.nguyendr <long.nguyendr@gmail.com>
2019-04-22 15:54:13 +0300

    OnInfoWindow Click event

--- app/src/main/java/com/example/worldgreen/MainActivity.java
+++ app/src/main/java/com/example/worldgreen/MainActivity.java
@@ -22,7 +22,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button btnSignOut, btnCreateReport, btnViewProfile, btnViewAllReport, btnViewAllEvent, btnDonate, btnMaps;
     private FirebaseAuth.AuthStateListener authListener;
@@ -50,59 +50,21 @@ public class MainActivity extends AppCompatActivity {
 
         //get current user
         final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        btnMaps = findViewById(R.id.maps_button);
+//        btnMaps = findViewById(R.id.maps_button);
+//        btnMaps.setOnClickListener(this);
         btnSignOut = findViewById(R.id.Logout_button);
+        btnSignOut.setOnClickListener(this);
         btnViewAllReport = findViewById(R.id.view_all_report_button);
+        btnViewAllReport.setOnClickListener(this);
         btnViewAllEvent = findViewById(R.id.view_all_event_button);
+        btnViewAllEvent.setOnClickListener(this);
         btnDonate = findViewById(R.id.donate_button);
+        btnDonate.setOnClickListener(this);
         btnCreateReport = findViewById(R.id.create_report_button);
+        btnCreateReport.setOnClickListener(this);
         btnViewProfile = findViewById(R.id.profile_button);
-        btnSignOut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                auth.signOut();
-                Toast.makeText(MainActivity.this, "Sign out Successful", Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(MainActivity.this, LoginActivity.class));
-                finish();
-            }
-        });
-        btnCreateReport.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, CreateReportActivity.class ));
-            }
-        });
+        btnViewProfile.setOnClickListener(this);
 
-        btnViewProfile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, ProfileActivity.class));
-            }
-        });
-        btnViewAllEvent.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, AllEventActivity.class));
-            }
-        });
-        btnViewAllReport.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, AllReportActivity.class));
-            }
-        });
-        btnDonate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, DonateActivity.class));
-            }
-        });
-        btnMaps.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, MapsActivity.class));
-            }
-        });
     }
 
     @Override
@@ -122,6 +84,37 @@ public class MainActivity extends AppCompatActivity {
         super.onStop();
         if (authListener != null) {
             auth.removeAuthStateListener(authListener);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.Logout_button:
+                auth.signOut();
+                Toast.makeText(MainActivity.this, "Sign out Successful", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                finish();
+                break;
+            case R.id.create_report_button:
+                startActivity(new Intent(MainActivity.this, CreateReportActivity.class ));
+                break;
+            case R.id.view_all_report_button:
+                startActivity(new Intent(MainActivity.this, AllReportActivity.class));
+                break;
+            case R.id.view_all_event_button:
+                startActivity(new Intent(MainActivity.this, AllEventActivity.class));
+                break;
+            case R.id.profile_button:
+                startActivity(new Intent(MainActivity.this, ProfileActivity.class));
+                break;
+            case R.id.donate_button:
+                startActivity(new Intent(MainActivity.this, DonateActivity.class));
+                break;
+//            case R.id.maps_button:
+//                startActivity(new Intent(MainActivity.this, MapsActivity.class));
+//                break;
+                default:break;
         }
     }
 }
